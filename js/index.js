f3fd4f7039e6fa985b80b81d8164a12490898b24
YourContender <dream-boy123@ukr.net>
2023-01-19 02:18:22 +0200

    final

--- js/index.js
+++ js/index.js
@@ -2,14 +2,39 @@ const button = document.getElementById('btn');
 const color = document.getElementById('color');
 
 function changeBgColor() {
-    let randomColor = color.textContent = '#' + (Math.random().toString(16) + '000000')
+    let randomColor = generationRandomColor();
+    
+    document.body.style.backgroundColor = randomColor    
+    
+    return randomColor;
+}
+
+//перший варіант
+function generationRandomColor() {
+    return color.textContent = '#' + (Math.random().toString(16) + '000000')
         .split('')
         .slice(2, 8)
         .join('')
         .toUpperCase()
-    
-    document.body.style.backgroundColor = randomColor    
-    
+}
+
+//другий варіант
+function generationRandomColor2() {
+    return color.textContent = '#' + (Math.random().toString(16) + '000000')
+        .substring(2, 8)
+        .toUpperCase()
+}
+
+//третій варіант
+function generationRandomColor3() {
+    let randomColor = '#';
+    let hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+
+    for (let i = 0; i < 6; i++) {
+        randomColor += hex[Math.floor(Math.random() * hex.length)];
+    }
+    color.textContent = randomColor;   
+
     return randomColor;
 }
 
